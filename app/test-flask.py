404e3255af0d7ea50153f24fdce520b65aa76472
kenanGonnot <78441094+kenanGonnot@users.noreply.github.com>
2022-02-12 17:13:35 +0100

    docker and flask working - add some html pages with stylesheets

--- app/test-flask.py
+++ app/test-flask.py
@@ -1,15 +1,35 @@
-from flask import Flask, jsonify, render_template, request, flash
+import os
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/test/')
+def test():
+    return render_template('test.html', content=["hugo", "patrik", "dandy"], r=2)
+
+
+@app.route('/verification')
+def verification():
+    return render_template('verification.html')
+
+
+@app.route('/identification')
+def identification():
+    return render_template('identification.html')
+
+
+# @app.route('/admin')
+# def admin():
+#     return redirect(url_for('/home'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
-
-
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
